74d0efc41dfdf44953f990403738fe794a07b280
Omar Crosby <omar.crosby@gmail.com>
2024-10-04 07:55:27 -0400

    chore: trigger a change to the hello lambda

--- lambdas/hello/handler.py
+++ lambdas/hello/handler.py
@@ -3,7 +3,13 @@ A simple lambda function that returns a message.
 """
 
 def lambda_handler(event: any, context: any):
-    """Sample Lambda function that returns 'Hello, World!'"""
+    """
+    Sample Lambda function that returns 'Hello, World!'
+
+    :param event: The event object
+    :param context: The context object
+    :return: A JSON object with 'Hello, World!' message
+    """
     print(f"Event: {event}")
     print(f"Context {context}")
 
